f5f929d5562dd403bb4632565f2c4807d656a6cf
esunshine1999 <143804923+esunshine1999@users.noreply.github.com>
2023-11-10 01:08:04 +0100

    Add files via upload

---
+++ light.js
@@ -0,0 +1,10 @@
+document.getElementById("iconmoon").onclick = function(){
+    document.getElementById("everything").style.backgroundColor = 'black'
+    document.getElementById("heading").style.color= 'brown'
+ }
+
+ document.getElementById("iconsun").onclick = function(){
+    document.getElementById("everything").style.backgroundColor = 'brown'
+    document.getElementById("heading").style.color = 'black'
+ }
+ 
